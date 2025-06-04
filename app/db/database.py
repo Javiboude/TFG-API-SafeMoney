3b9414311034d2e24a379b3995e4d3d5abc13cdf
Javier <javierboudere@gmail.com>
2025-06-04 17:05:05 +0200

    Add: cambio base de datos

--- app/db/database.py
+++ app/db/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost/usuariosTFG"
+DATABASE_URL = "mysql+pymysql://tfguser:Tfg123@456@<13.53.206.151>/usuariosTFG"
 
 engine = create_engine(DATABASE_URL, connect_args={"charset": "utf8mb4"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
